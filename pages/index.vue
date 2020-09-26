fd9eb9e238caf833afe68f472516d6e28555f2b9
Tomoharu Watanabe <watatomo1216@gmail.com>
2020-09-26 21:04:08 +0900

    URL関連のミスを修正

--- pages/index.vue
+++ pages/index.vue
@@ -13,11 +13,11 @@ import axios from 'axios'
 export default {
   async asyncData (context) {
     const timeUrl = 'https://friendly-yalow-4a2e2c.netlify.app/.netlify/functions/hello'
-    const timeText = await axios.get(url)
+    const timeText = await axios.get(timeUrl)
       .then((res) => { return res.data })
       .catch(() => { return '' })
     const xmlUrl = 'https://friendly-yalow-4a2e2c.netlify.app/.netlify/functions/note'
-    const xmlText = await axios.get(url)
+    const xmlText = await axios.get(xmlUrl)
       .then((res) => { return res.data })
       .catch(() => { return '' })
     return { timeText, xmlText }
