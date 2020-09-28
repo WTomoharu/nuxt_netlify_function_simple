cd1fb0bc499b920896137716b97e0568fd5bd91b
Tomoharu Watanabe <watatomo1216@gmail.com>
2020-09-29 02:26:56 +0900

    xml2jsの追加とrssでのリストを実装完了

--- pages/index.vue
+++ pages/index.vue
@@ -2,13 +2,23 @@
   <v-layout>
     <v-flex class="text-center">
       <h1>{{ timeText }}</h1>
-      <h1>{{ xmlText }}</h1>
+      <!-- <h1>{{ xmlText }}</h1> -->
+      <ul>
+        <li
+          v-for="item in xmlTitleItems"
+          :key="item"
+          style="text-align: left;"
+        >
+          {{ item }}
+        </li>
+      </ul>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
 import axios from 'axios'
+import xml2js from 'xml2js'
 
 export default {
   async asyncData (context) {
@@ -16,11 +26,25 @@ export default {
     const timeText = await axios.get(timeUrl)
       .then((res) => { return res.data })
       .catch(() => { return '' })
+
     const xmlUrl = 'https://friendly-yalow-4a2e2c.netlify.app/.netlify/functions/note'
     const xmlText = await axios.get(xmlUrl)
       .then((res) => { return res.data })
       .catch(() => { return '' })
-    return { timeText, xmlText }
+
+    const parser = new xml2js.Parser({
+      async: false,
+      explicitArray: false
+    })
+
+    let xmlObj = null
+    parser.parseString(xmlText, (_, json) => {
+      xmlObj = json
+    })
+
+    const xmlTitleItems = xmlObj.rss.channel.item.map(v => v.title)
+
+    return { timeText, xmlText, xmlTitleItems }
   }
 }
 </script>
