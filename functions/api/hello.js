7bfc5e0a0e329a0a2850f5ce806a33fcab00741a
Tomoharu Watanabe <watatomo1216@gmail.com>
2020-10-04 17:49:00 +0900

    hello.jsのクロスドメイン制約を解除

--- functions/api/hello.js
+++ functions/api/hello.js
@@ -2,6 +2,7 @@ exports.handler = async (event, context) => {
   const timeText = new Date().toLocaleString()
   return {
     statusCode: 200,
+    headers: { 'Access-Control-Allow-Origin': '*' },
     body: `Hello, World ${timeText}`
   }
 }
